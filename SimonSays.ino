6b74a1f97a878cecf8c0ca1644d0479e6b9265c7
JeBear76 <jerepondumie@gmail.com>
2017-07-08 21:32:13 +0200

    working program

--- SimonSays.ino
+++ SimonSays.ino
@@ -1,90 +1,138 @@
-int sequence[100];
-int currentLength = 0;
-int inputCount = 0;
-int lastInput = 0;
-int expectedRead = 0;
-bool buttonPressed = false;
-bool waiting = false;
+byte sequence[100];
+byte curLen = 0;
+byte inputCount = 0;
+byte lastInput = 0;
+byte expRd = 0;
+bool btnDwn = false;
+bool wait = false;
 bool resetFlag = false;
+byte pins[] = {2, 13, 10, 8};
+byte soundPin = 5;
+byte noPins = 4;
+long inputTime = 0;
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
+  delay(5000);
+  Serial.begin(9600);
   Reset();
 }
 
-void Reset(){
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  Serial.println("Reset");
-  for(int i = 0; i < 10; i++){
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    delay(250);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    delay(250);
+void setPinDirection(byte dir){
+  for(byte i = 0; i < noPins; i++){
+    pinMode(pins[i], dir); 
   }
-  currentLength = 0;
+}
+
+void writeAllPins(byte val){
+  for(byte i = 0; i < noPins; i++){
+    digitalWrite(pins[i], val); 
+  }
+}
+
+void beep(byte freq){
+  analogWrite(soundPin, 2);
+  delay(freq);
+  analogWrite(soundPin, 0);
+  delay(freq);
+}
+
+void flash(short freq){
+  setPinDirection(OUTPUT);
+  for(int i = 0; i < 5; i++){
+    writeAllPins(HIGH);
+    beep(50);
+    delay(freq);
+    writeAllPins(LOW);
+    delay(freq);
+  }
+}
+
+void Reset(){
+  flash(500);
+  curLen = 0;
   inputCount = 0;
   lastInput = 0;
-  expectedRead = 0;
-  buttonPressed = false;
-  waiting = false;
+  expRd = 0;
+  btnDwn = false;
+  wait = false;
   resetFlag = false;
 }
 
-void loop() {
-  if(!waiting){
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    sequence[currentLength] = random(5,7);
-    currentLength++;
-    for(int i = 0; i < currentLength; i++){
+void Lose(){
+  flash(50);  
+}
+
+void playSequence(){
+  for(int i = 0; i < curLen; i++){
+      Serial.print("Seq: ");
+      Serial.print(i);
+      Serial.print("Pin: ");
       Serial.println(sequence[i]);
       digitalWrite(sequence[i], HIGH);
       delay(500);
       digitalWrite(sequence[i], LOW);
       delay(250);
-    }    
-    waiting = true;
+    } 
+}
+
+void DoLoseProcess(){
+  Lose();
+  delay(1000);
+  playSequence();
+  delay(1000);
+  Reset();
+}
+
+void loop() {
+  if(!wait){
+    setPinDirection(OUTPUT);
+    sequence[curLen] = pins[random(0,noPins)];
+    curLen++;
+    playSequence();   
+    wait = true;    
+    beep(50);
+    inputTime = millis();
   }else{
-    pinMode(5, INPUT);
-    pinMode(6, INPUT);
-    if(!buttonPressed){
-      expectedRead = sequence[inputCount];
-      Serial.print("Expected ");
-      Serial.println(expectedRead);
-      for(int i = 5; i <= 6; i++){
-        if(i==expectedRead)
+    if(millis() - inputTime > 2000){
+      DoLoseProcess();
+      return;
+    }      
+    setPinDirection(INPUT);
+    if(!btnDwn){
+      expRd = sequence[inputCount];
+      Serial.print("Expected: ");
+      Serial.println(expRd);
+      for(int i = 0; i <= noPins; i++){
+        if(pins[i]==expRd)
           continue;
-        if(digitalRead(i) == 1){
-          Serial.print("Read ");
-          Serial.println(i);
-          lastInput = i;
+        if(digitalRead(pins[i]) == 1){
+          lastInput = pins[i];
+          Serial.print("Read: ");
+          Serial.println(lastInput);
           resetFlag = true;
-          buttonPressed = true;
+          btnDwn = true;
         }
       }
     }
-    if(digitalRead(expectedRead) == 1 && !buttonPressed)
+    if(digitalRead(expRd) == 1 && !btnDwn)
     {
-      Serial.print("Read ");
-      Serial.println(expectedRead);
-      lastInput = expectedRead;
+      inputTime = millis();
+      lastInput = expRd;
+      Serial.print("Read: ");
+      Serial.println(lastInput);
       inputCount++;
-      buttonPressed = true;
+      btnDwn = true;
     }else{
-      if(buttonPressed && digitalRead(lastInput) == 0){
-        buttonPressed = false;
-        if(resetFlag)
-          Reset();
+      if(btnDwn && digitalRead(lastInput) == 0){
+        btnDwn = false;
+        if(resetFlag){
+          DoLoseProcess(); 
+        }
         else{
-          if(inputCount == currentLength){
-            waiting = false;
+          if(inputCount == curLen){
+            wait = false;
             inputCount = 0;   
-            delay(2000);
+            delay(1500);
           }
         }
       }
